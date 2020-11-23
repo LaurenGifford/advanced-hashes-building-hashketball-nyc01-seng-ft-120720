59894a0e89907895260e3cb3b0349bf07ded8d7b
Lauren Gifford <lauren.gifford.17@gmail.com>
2020-11-23 20:07:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,7 +2,27 @@
 require 'pry'
 
 def game_hash
-  :home => {}
-  :away => {}
+  :home => {
+    :team_name => "Brooklyn Nets",
+    :colors => ["Black", "White"],
+    :players => [
+      {:player_name => "Alan Anderson", :number => "0", :shoe => "16", :points => "22", :rebounds => "12", :assists => "12", :steals => "3", :blocks => "1", :slam_dunks => "1"},
+      {:player_name => stat, :number => stat, :shoe => stat, :points => stat, :rebounds => stat, :assists => stat, :steals => stat, :blocks => stat, :slam_dunks => stat},
+      {:player_name => stat, :number => stat, :shoe => stat, :points => stat, :rebounds => stat, :assists => stat, :steals => stat, :blocks => stat, :slam_dunks => stat},
+      {:player_name => stat, :number => stat, :shoe => stat, :points => stat, :rebounds => stat, :assists => stat, :steals => stat, :blocks => stat, :slam_dunks => stat},
+      {:player_name => stat, :number => stat, :shoe => stat, :points => stat, :rebounds => stat, :assists => stat, :steals => stat, :blocks => stat, :slam_dunks => stat}
+      ]
+  }
+  :away => {
+    :team_name => "Charlotte Hornets",
+    :colors => ["Turquoise", "Purple"],
+    :players => [
+      {:player_name => stat, :number => stat, :shoe => stat, :points => stat, :rebounds => stat, :assists => stat, :steals => stat, :blocks => stat, :slam_dunks => stat},
+      {:player_name => stat, :number => stat, :shoe => stat, :points => stat, :rebounds => stat, :assists => stat, :steals => stat, :blocks => stat, :slam_dunks => stat},
+      {:player_name => stat, :number => stat, :shoe => stat, :points => stat, :rebounds => stat, :assists => stat, :steals => stat, :blocks => stat, :slam_dunks => stat},
+      {:player_name => stat, :number => stat, :shoe => stat, :points => stat, :rebounds => stat, :assists => stat, :steals => stat, :blocks => stat, :slam_dunks => stat},
+      {:player_name => stat, :number => stat, :shoe => stat, :points => stat, :rebounds => stat, :assists => stat, :steals => stat, :blocks => stat, :slam_dunks => stat}
+      ]
+  }
   
 end
