4adc9cf08847b99ddb0c938d09a27c9ae44c1ebd
Lauren Gifford <lauren.gifford.17@gmail.com>
2020-11-23 20:14:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -10,11 +10,10 @@ def game_hash
       {:player_name => "Alan Anderson", :number => "0", :shoe => "16", :points => "22", :rebounds => "12", :assists => "12", :steals => "3", :blocks => "1", :slam_dunks => "1"},
       {:player_name => "stat", :number => "stat", :shoe => "", :points => "", :rebounds => "", :assists => "", :steals => "", :blocks => "", :slam_dunks => ""},
       {:player_name => "stat", :number => "stat", :shoe => "", :points => "", :rebounds => "", :assists => "", :steals => "", :blocks => "", :slam_dunks => ""},
-      {:player_name => "stat", :number => "stat", :shoe => "", :points => "", :rebounds => "", :assists => "", :steals => "", :blocks => "", :slam_dunks => ""t},
+      {:player_name => "stat", :number => "stat", :shoe => "", :points => "", :rebounds => "", :assists => "", :steals => "", :blocks => "", :slam_dunks => ""},
       {:player_name => "stat", :number => "stat", :shoe => "", :points => "", :rebounds => "", :assists => "", :steals => "", :blocks => "", :slam_dunks => ""}
       ]
-  }
-  ,
+  },
   :away => {
     :team_name => "Charlotte Hornets",
     :colors => ["Turquoise", "Purple"],
